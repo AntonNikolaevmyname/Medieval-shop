97295afebd2e14dfc6367d1a264789937ade3e8e
AntonNikolaev <34309871+AntonNikolaevKazan@users.noreply.github.com>
2020-01-30 07:21:09 +0300

    Create game speed changer

---
+++ Medieval-shop-2020/Assets/Scripts/SomeTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeTests : MonoBehaviour
+{
+    [Range(0,5)]
+    [TooltipAttribute("0 пауза")]
+    public float tS = 1f;
+
+    void Update()
+    {
+        Time.timeScale = tS;
+    }
+}
